ac3b90b33648a59ced90e99cdd14bafcf00b0d1e
mayur chakalasiya <mayur.chakalasiya1990@gmail.com>
2020-08-18 10:46:02 +0530

    Merge sort Algorithm with different variation

---
+++ Datastructure & Algo/src/org/dsf/sortingalgo/IntersectionOfSortedArrays.java
@@ -0,0 +1,43 @@
+package org.dsf.sortingalgo;
+
+public class IntersectionOfSortedArrays {
+
+    /*
+            Time complexity: O(m+n)
+            Auxilary: O(1)
+
+            Pre-requisite : Both Arrays must be sorted
+
+     */
+
+    static void intersection(int[] a, int[] b,int n,int m){
+
+        int i=0,j=0;
+        while(i<n && j<m){
+
+            if(i>0 && a[i-1]==a[i]) {
+                i++;
+                continue;
+            }
+             if(a[i]<b[j]){
+                 i++;
+             }else if(a[i]>b[j]){
+                 j++;
+             }else{
+                 System.out.println(a[i]);
+                 i++;
+                 j++;
+             }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int a[]={3,5,10,10,10,5,15,20};
+        int b[]={5,10,10,15,30};
+
+        System.out.println("Intersection point of Sorted Arrays:");
+        intersection(a,b,a.length,b.length);
+    }
+
+}
