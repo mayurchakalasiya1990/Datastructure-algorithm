e28c431dea322b08b0b5e369749999ffb53c26b0
geekarea <mayur.chakalasiya1990@gmail.com>
2021-09-01 16:47:10 +0530

    Recursion problems solution

---
+++ Datastructure & Algo/src/org/dsf/recursion/Test.java
@@ -0,0 +1,16 @@
+package org.dsf.recursion;
+
+public class Test {
+
+    static void fun(int n){
+        if(n==0)
+            return;
+        System.out.println("before fun:"+(n-1));
+        fun(n-1);
+        System.out.println("After fun:"+(n-1));
+    }
+
+    public static void main(String[] args) {
+        fun(3);
+    }
+}
